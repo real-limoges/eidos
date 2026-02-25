78ef765dba06d70c75059145a75682575fab01d1
Real Limoges <b.real.limoges@gmail.com>
2026-02-25 13:07:47 -0800

    feat(05-03): register camera and surface_plot submodules with pub use in dataviz/mod.rs
    - Add pub use camera::{Camera, Point2D, Point3D, Vector3D}
    - Add pub use surface_plot::SurfacePlot
    - Reorder pub mod declarations alphabetically per plan spec

--- src/dataviz/mod.rs
+++ src/dataviz/mod.rs
@@ -1,12 +1,14 @@
 pub mod axes;
 pub mod camera;
-pub mod surface_plot;
 pub mod confidence_band;
 pub mod data_curve;
 pub(crate) mod spline;
 pub mod spline_fit;
+pub mod surface_plot;
 
 pub use axes::{Axes, AxisRange};
+pub use camera::{Camera, Point2D, Point3D, Vector3D};
 pub use confidence_band::ConfidenceBand;
 pub use data_curve::DataCurve;
 pub use spline_fit::SplineFit;
+pub use surface_plot::SurfacePlot;
